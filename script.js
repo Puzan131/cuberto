a890aade10aa3768e3fd8a255937861ab958bdd9
Poojan Bhatt <poojanbhatt392@gmail.com>
2023-09-19 22:52:31 +0545

    more changes

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-gsap.from(".navbar img, .navbar .right",{
+let cursor = document.querySelector(".cursor")
+document.querySelector(".container").addEventListener("mousemove",(dets)=>{
+    cursor.style.top = dets.y + "px"
+    cursor.style.left = dets.x + "px"
+})
+gsap.from(".navbar img, .navbar .right, .main h5, .main h4",{
    scale:0,
    opacity:0,
    duration:1,
@@ -6,15 +11,14 @@ gsap.from(".navbar img, .navbar .right",{
    ease:Circ.easeInOut
 })
 gsap.from(".main h1",{
-    x:"20vw",
+    y:"20vw",
     duration:1.5,
     delay:1,
     ease:Power2.easeInOut
 })
 gsap.from(".main h2",{
-    x:"-20vw",
+    y:"-40vw",
     duration:1.5,
     delay:1,
     ease:Power2.easeInOut
-
 })
