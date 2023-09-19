e7e5fd330c40f5ebed2c63e36a65dca897197d13
Poojan Bhatt <poojanbhatt392@gmail.com>
2023-09-19 22:04:40 +0545

    extra changes

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ gsap.from(".navbar img, .navbar .right",{
    scale:0,
    opacity:0,
    duration:1,
+   delay:1,
    ease:Circ.easeInOut
 })
 gsap.from(".main h1",{
     x:"20vw",
     duration:1.5,
+    delay:1,
     ease:Power2.easeInOut
 })
 gsap.from(".main h2",{
     x:"-20vw",
     duration:1.5,
+    delay:1,
     ease:Power2.easeInOut
 
 })
