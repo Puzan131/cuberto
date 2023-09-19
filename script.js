547d9b9e4bad3278b1242d37fd35808cfab0d990
Poojan Bhatt <poojanbhatt392@gmail.com>
2023-09-19 21:50:19 +0545

    added files

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.from(".navbar img, .navbar .right",{
+   scale:0,
+   opacity:0,
+   duration:1,
+   ease:Circ.easeInOut
+})
+gsap.from(".main h1",{
+    x:"20vw",
+    duration:1.5,
+    ease:Power2.easeInOut
+})
+gsap.from(".main h2",{
+    x:"-20vw",
+    duration:1.5,
+    ease:Power2.easeInOut
+
+})
