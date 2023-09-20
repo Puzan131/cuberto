69a7b02ada50461c2af838f0711e9a0fe9102ba9
Poojan Bhatt <poojanbhatt392@gmail.com>
2023-09-20 13:50:08 +0545

    more changes

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-let cursor = document.querySelector(".cursor")
-document.querySelector(".container").addEventListener("mousemove",(dets)=>{
-    cursor.style.top = dets.y + "px"
-    cursor.style.left = dets.x + "px"
-})
-gsap.from(".navbar img, .navbar .right, .main h5, .main h4",{
-   scale:0,
-   opacity:0,
-   duration:1,
-   delay:1,
-   ease:Circ.easeInOut
-})
-gsap.from(".main h1",{
-    y:"20vw",
-    duration:1.5,
-    delay:1,
-    ease:Power2.easeInOut
-})
-gsap.from(".main h2",{
-    y:"-40vw",
-    duration:1.5,
-    delay:1,
-    ease:Power2.easeInOut
-})
+let cursor = document.querySelector(".cursor");
+document.querySelector(".container").addEventListener("mousemove", (dets) => {
+  cursor.style.top = dets.y + "px";
+  cursor.style.left = dets.x + "px";
+});
+gsap.from(".navbar img, .navbar .right, .main h5, .main h4", {
+  scale: 0,
+  opacity: 0,
+  duration: 1,
+  delay: 1,
+  ease: Circ.easeInOut,
+});
+gsap.from(".main h1", {
+  y: "20vw",
+  duration: 1.5,
+  delay: 1,
+  ease: Power2.easeInOut,
+});
+gsap.from(".main h2", {
+  y: "-40vw",
+  duration: 1.5,
+  delay: 1,
+  ease: Power2.easeInOut,
+});
+document.querySelector(".main-text").addEventListener("mousemove", (dets) => {
+  document.querySelector(".elem1 img").style.scale = "1";
+  document.querySelector(".elem1 img").style.opacity = "1";
+  document.querySelector(".elem1 img").style.top = dets.y - 100 + "px";
+  document.querySelector(".elem1 img").style.left = dets.x - 100 + "px";
+  cursor.style.opacity = "0";
+});
+document.querySelector(".main-text").addEventListener("mouseleave", () => {
+  document.querySelector(".elem img").style.scale = "0";
+  document.querySelector(".elem img").style.opacity = "0";
+  cursor.style.opacity = "1";
+});
